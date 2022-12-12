10a03686016dc1e0ea4e0d4a19a9fc42c1879864
TAN MIN ZHE 2103381F <2103381F@student.tp.edu.sg>
2022-12-12 22:23:28 +0800

    Added registration and login

---
+++ DatingApp/Server/Data/Migrations/20221212140102_addNameToUser.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.Server.Data.Migrations
+{
+    public partial class addNameToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+            migrationBuilder.AddColumn<string>(
+                name: "ICNumber",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "AspNetUsers");
+            migrationBuilder.DropColumn(
+                name: "ICNumber",
+                table: "AspNetUsers");
+        }
+    }
+}
--- DatingApp/Server/Models/ApplicationUser.cs
+++ DatingApp/Server/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using DatingApp.Shared.Domain;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,11 @@ namespace DatingApp.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string ICNumber { get; set; }
+        public virtual PersonalProfile PersonalProfile { get; set; }
     }
 }
